11f745c36e8c14ea1ab57b4303694db108fab426
kamil-zych <kamil.zych@schibsted.pl>
2016-03-17 11:17:20 +0100

    infra refactor

---
+++ infrastructure/base.js
@@ -0,0 +1,32 @@
+var heroin = require('heroin-js');
+
+var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
+
+var base = {
+    region: 'eu',
+    maintenance: false,
+    stack: 'cedar-14',
+    config_vars: {
+        MONGOLAB_URI: process.env.MONGOLAB_URI,
+        NAME: 'kamil'
+    },
+    addons: {},
+    collaborators: ['wojciech.iskra@schibsted.pl',
+        'titzmateusz@gmail.com',
+        'kzych.loop@gmail.com'],
+    features: {
+        'runtime-dyno-metadata': {enabled: false},
+        'log-runtime-metrics': {enabled: false},
+        'http-session-affinity': {enabled: false},
+        preboot: {enabled: false},
+        'http-shard-header': {enabled: false},
+        'http-end-to-end-continue': {enabled: false}
+    },
+    formation: [{process: 'web', quantity: 1, size: 'Free'}],
+    log_drains: []
+};
+
+module.exports = {
+    config: base, 
+    configurator : configurator
+};
--- infrastructure/test.js
+++ infrastructure/test.js
@@ -2,10 +2,10 @@ var _ = require('lodash')
 var baseConfig = require('./base').config;
 var configurator = require('./base').configurator;
 
-var prodConfig = {
-    name: 'book-inventory-kz',
+var testConfig = {
+    name: 'book-inventory-kz-test',
     log_drains: []
 };
 
-var config = _.merge({}, baseConfig, prodConfig)
+var config = _.merge({}, baseConfig, testConfig);
 configurator(config);
